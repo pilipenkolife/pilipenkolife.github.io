98b1da3181ed133801fd80b7a8829a188e470877
pilipenkolife <pilipenkolife@gmail.com>
2016-11-09 11:22:31 +0200

    soonpure

---
+++ soonpure/js/myscript.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  $(".anchbut1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+
+    $(".click-but").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+
+});
